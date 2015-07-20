015a9f65a79bd2a0a63f5f02f128872d9c87ed9c
zhangxistudy11 <qianfeng@zzxx.local>
2015-07-20 16:56:03 +0800

    gibDemo-1

---
+++ 网页ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网页ceshi
+//
+//  Created by qianfeng on 15-7-10.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 网页ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网页ceshi
+//
+//  Created by qianfeng on 15-7-10.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
